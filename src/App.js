2302aed5b97c10ff18679c069f0990c40fa8f477
Haarissams <haarissams02@gmail.com>
2024-09-30 13:43:20 +0530

    commit all

--- src/App.js
+++ src/App.js
@@ -1,41 +1,68 @@
 import React from 'react';
 import PriceCard from './components/PriceCard';
-import './components/PriceCard.css'; 
+import './App.css'; // Import the updated App.css file
 
 const App = () => {
-
-  const plans = [
+  const pricingPlans = [
     {
-      plan: 'Basic Plan',
-      price: '$10/month',
-      features: ['Single User', '1.5GB/Day', 'Unlimited Calls'],
+      plan: 'Free',
+      price: '$0/month',
+      features: [
+        'Single User',
+        '50GB Storage',
+        'Unlimited Public Projects',
+        'Community Access',
+        'No Dedicated Phone Support',
+        'No Free Subdomain',
+        'No Monthly Status Reports'
+      ],
       isPopular: false
     },
     {
-      plan: 'Standard Plan',
-      price: '$20/month',
-      features: ['5 User', '2GB/Day', 'Unlimited Calls'],
+      plan: 'Plus',
+      price: '$9/month',
+      features: [
+        '5 Users',
+        '50GB Storage',
+        'Unlimited Public Projects',
+        'Community Access',
+        'Dedicated Phone Support',
+        'Free Subdomain',
+        'No Monthly Status Reports'
+      ],
       isPopular: true
     },
     {
-      plan: 'Premium Plan',
-      price: '$30/month',
-      features: ['10 User', '2.5GB/Day', 'Unlimited Calls'],
+      plan: 'Pro',
+      price: '$49/month',
+      features: [
+        'Unlimited Users',
+        '50GB Storage',
+        'Unlimited Public Projects',
+        'Community Access',
+        'Unlimited Private Projects',
+        'Dedicated Phone Support',
+        'Free Subdomain',
+        'Monthly Status Reports'
+      ],
       isPopular: false
     }
   ];
 
   return (
-    <div className="app">
-      {plans.map((plan, index) => (
-        <PriceCard
-          key={index}
-          plan={plan.plan}
-          price={plan.price}
-          features={plan.features}
-          isPopular={plan.isPopular}
-        />
-      ))}
+    <div className="pricing-table">
+      <h1>React Price Card Task</h1>
+      <div className="pricing-cards">
+        {pricingPlans.map((plan, index) => (
+          <PriceCard
+            key={index}
+            plan={plan.plan}
+            price={plan.price}
+            features={plan.features}
+            isPopular={plan.isPopular}
+          />
+        ))}
+      </div>
     </div>
   );
 };
